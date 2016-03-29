17d9bcb7d18aea89f648bf29758dc4b183c74ab1
Jazzy <jasmina.gasper@gmail.com>
2016-03-29 16:52:33 +0100

    airport storage

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -11,4 +11,26 @@ describe('Airport', function(){
       expect(airport.capacity).toEqual(20);
     });
   });
+
+  describe('#storage', function(){
+    it('should start with empty storage', function(){
+      expect(airport.storage).toEqual([]);
+    });
+  });
+
+  describe('#allow landing', function(){
+    it('should allow aircraft to land', function(){
+      storageStatus = airport.storage.length
+      airport.allowLanding();
+      expect(airport.storage.length).toEqual(storageStatus+1)
+    });
+    it('should raise an error when airport is over capacity', function(){
+      for (i=1; i <= 20; i++){
+        airport.allowLanding();
+      };
+      expect(function(){
+        airport.allowLanding();
+      }).toThrowError('airport is over capacity');
+    });
+  });
 });
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -1,3 +1,11 @@
 function Airport(){
   this.capacity = 20;
+  this.storage = [];
+}
+
+Airport.prototype.allowLanding = function(plane) {
+  if (this.storage.length === this.capacity){
+    throw new Error('airport is over capacity');
+  }
+  this.storage.push(plane)
 }
